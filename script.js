529aa1e09435c6efaced9357e79e76fe0c4c31ca
Yoshifumi-T <2g.4423@gmail.com>
2020-05-15 19:54:28 +0900

    スタイル等調整

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
 'usestrict'
 
 const elements = [];
-attachEvent();
+attachEventForRange();
+attachEventForMain();
 
-function attachEvent() {
+// レンジスライダーの値を表示する為のイベントを追加する
+function attachEventForRange() {
+    const range   = document.querySelector('.range-input');
+
+    if(!range) return;
+
+    range.addEventListener('mousedown', displayRangeValue);
+    range.addEventListener('mouseup', hideRangeValue);
+    document.addEventListener('mousemove', changeRangeValue);
+}
+
+function displayRangeValue() {
+    const value = document.querySelector('.range-value');
+    
+    if(!value) return;
+
+    value.style.display = 'block';
+}
+
+function hideRangeValue() {
+    const value = document.querySelector('.range-value');
+  
+    if(!value) return;
+  
+    value.style.display = 'none';
+}
+
+// マウスにあわせて要素を動かす
+function changeRangeValue(event) {
+    const range = document.querySelector('.range-input');
+    const value = document.querySelector('.range-value');
+
+    if(!range || !value) return;
+    
+    const x = event.offsetX;
+    const e = document.querySelector('.range-value');
+    e.innerHTML = `${range.value}px`;
+    e.style.left = `${x}px`;
+}
+
+// メイン機能のためのイベントを追加する
+function attachEventForMain() {
     const pushBtn = document.querySelector('.push-btn');
-    const popBtn = document.querySelector('.pop-btn');
+    const popBtn  = document.querySelector('.pop-btn');
+
     if(!pushBtn || !popBtn) return;
+
     pushBtn.addEventListener('click', push);
     popBtn.addEventListener('click', pop);
 }
@@ -23,7 +67,7 @@ function pop() {
 
 // 要素を追加する
 function pushElement() {
-    const target = document.querySelector('#js-target');
+    const target = document.querySelector('.circle');
     if(!target) return;
     const e = document.createElement('div');
     const size = getElementSize();
@@ -34,9 +78,9 @@ function pushElement() {
 }
 
 function getElementSize() {
-    const target = document.querySelector('.elm-size');
-    if(!target) return 0;
-    return Number(target.value);
+    const range = document.querySelector('.range-input');
+    if(!range) return 0;
+    return Number(range.value);
 }
 
 // 要素の配置を整形する
@@ -44,15 +88,15 @@ function adjustElement() {
     elements.forEach((e, i) => {
         const pos = getPosition(i);
         e.style.position = 'absolute';
-        e.style.left = `${pos[0]}px`;
-        e.style.top = `${pos[1]}px`;
+        e.style.left = `${pos.posX}px`;
+        e.style.top = `${pos.posY}px`;
         e.style.transform = 'translate(-50%, -50%)'
     });
 }
 
 // 要素の座標を計算する
 function getPosition(i) {
-    const target = document.querySelector('#js-target');
+    const target = document.querySelector('.circle');
     if(!target) return;
 
     // 半径を取得する
@@ -66,7 +110,7 @@ function getPosition(i) {
     const x = Math.cos(d * Math.PI / 180) * r + r;
     const y = Math.sin(d * Math.PI / 180) * r + r;
 
-    return [x, y];
+    return {posX: x, posY: y};
 }
 
 // 最後に追加した要素を削除する
@@ -81,5 +125,3 @@ function popElement() {
 function isLastElement(i) {
     return i === elements.length - 1;
 }
-
-
